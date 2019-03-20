ea2bf05bfeb8d5da7e0df2207957fbcc2bccec39
amber880311 <amber880311@gmail,com>
2019-03-21 03:15:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    int size=30;
+
+    public void bigger(View v) {
+        TextView textview;
+        textview=(TextView)findViewById(R.id.textview);
+        textview.setTextSize(++size);
+    }
+}
